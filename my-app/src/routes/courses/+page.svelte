2bb85c3c3f645ce3d9115206d19db3cf031359bf
Sophie-skyyye <131939901+Sophie-skyyye@users.noreply.github.com>
2026-09-22 18:44:32 +1200

    Add my-app/src/routes/courses/+page.svelte

---
+++ my-app/src/routes/courses/+page.svelte
@@ -0,0 +1,126 @@
+<section class="coursespage">
+    <section class="info">
+    <h1>INFORMATION</h1>
+    <div class="Box">
+         <p>Tambotie offers exciting practice-based and accredited FGASA courses that <br>
+     introduce you to a wonderful world of tracks, signs and trailing of Africa’s <br>
+     wild animals. We also offer a wildlife vet nursing placement course. <br>
+     Helping with the animals at the rehab along with going into the field <br>
+     to farm calls of poaching. Whether you are pursuing a career in <br>
+     conservation or looking to further your knowledge as a nature enthusiast. <br>
+     Tambotie has the perfect course for you</p>
+    </div>
+   
+     <img src="Images/blog.jpg" alt="Background">
+
+     <h3>Our Courses</h3>
+     <div class="Box">
+        <ul>
+            <li>Tracking 11 - 17 April</li>
+            <li>Tracking 20 - 26 June</li>
+            <li>Tracking 15 - 21 August</li>
+            <li >Trailing 6 - 12 June</li>
+            <li>Vet nursing 23 Feb - 26 July</li>
+            <li>Vet nursing 28 July - 15 December </li>
+            <li>Vet nursing 23 Feb - 15 December </li>
+        </ul>
+     </div>
+     
+
+     <p>
+        The courses are designed to benefit both the enthusiastic observer as well as the serious conservationist <br>
+        looking to further their expertise. While on course you might get the opportunity to trial some of Africa’s <br>
+        Big Five like Buffalo, Rhino and Leopard. Sleep-out under the African stars are a big highlight of the <br>
+        course, but be warned – sleeping usually isn’t much of a priority… <br>
+<br>
+        Colin Patrick conducts and oversees all courses. He is an accredited FGASA trainer and assessor. As a field <br>
+        guide, he has achieved the top qualification of “scout” and is also a senior tracker and external evaluator <br>
+        with over 20 years of experience in the conservation industry. Colin heads up the Moholoholo team who are <br>
+        extremely passionate about teaching and sharing their knowledge and experiences. <br>
+        Minimum age 16 years. <br>
+<br>
+        We have proudly joined with the University of Pretoria and their Vet nurse programme. <br> 
+        first and second year students can work with us for part of the year while third <br>
+        years spend the whole year with us. <br>
+        This option is very new, only being introduced 2 years ago. Theres lots for both <br>
+        the students and us to learn during this amazing opportunity. <br>
+        Dr Marisnne Snyman and Dr Grant Neil will be directing this placement.
+     </p>
+</section>
+
+<section class="recognition">
+    <h1>RECOGNITION</h1>
+    <div class="Box">
+       <p>Our tracking courses are recognized by Cyber tracker.
+           Cyber tracker is a tracking software that gathers data <br>
+           from profesonal trackers. Cyber tracker tracks 
+           Students who successfully pass the final practical <br>
+           evaluation are registered with Cyber Tracker and <br>
+           their names are published on the Cyber Tracker website.
+        </p> 
+    </div>
+    
+</section>
+
+<section class="courseLocation">
+    <h1>LOCATION AND HOUSEING</h1>
+    <div class="Box">
+       <p>Our courses are conducted on the Tambootie Game Reserves, which lie <br>
+        at the foot of the mighty Drakensburg in the Lowveld region of <br>
+        South Africa’s Limpopo Province. Attendees are accommodated in <br>
+        comfortable thatched chalets which have all the standard appliances <br>
+        and en suite bathroom consisting of a shower, toilet and wash basin. <br>
+        </p> 
+    </div>
+        <img class="sceneimg" src="Images/background.jpg" alt="Background">
+</section>
+
+<section class="FAQs">
+    <h1>FAQs</h1>
+    <h3>Do I Need Any Prior Tracking Experience?</h3>
+    <p>No prior tracking experience is required to enrol in the course. Our dedicated instructors are committed to providing comprehensive <br>
+        training, ensuring that you can confidently engage in tracking wildlife even if you’re starting from scratch.</p>
+    
+    <h3>What Will I Learn In The Tracking Course?</h3>
+    <p>Throughout the course, you will gain the skills to track endangered species like the African wild dog and even elusive animals such <br>
+        as the black rhino. You’ll dive into both practical tracking techniques and essential theoretical knowledge that covers a range of <br>
+        topics, from traditional tracking methods to wildlife monitoring and conservation management.</p>
+
+    <h3>Will I Have The Chance To Work With Experienced Trackers During The Course?</h3>
+    <p>Absolutely! Our course provides a unique opportunity to learn from experienced trackers who possess extensive field knowledge. <br>
+        These skilled trackers will not only mentor you but also share their insights and expertise, enhancing your animal tracking abilities.</p>
+</section>
+</section>
+
+<style>
+    .coursespage{
+        text-align: center;
+    }
+    .Box{
+        background: #fae18f;
+        border-radius: 10px;
+        box-sizing: border-box;
+        width: 30%;
+        margin: 0 auto; /* Center align the box horizontally */
+        text-align: left; 
+    }
+    img {
+        padding-top: 10px;
+    }
+
+    .Box ul li{
+        list-style: none;
+    }
+
+    @media (max-width: 800px) {
+   
+   .Box{
+       width: 100%;
+   }
+
+   .coursespage img{
+   width: 100%; /* Set image width to 100% to match the size of the screen*/
+   }
+}
+
+</style>

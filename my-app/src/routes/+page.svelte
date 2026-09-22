65e043bb5cccb8da9fdfeba2f4211ade2e971857
Sophie-skyyye <131939901+Sophie-skyyye@users.noreply.github.com>
2026-09-22 18:43:19 +1200

    Add my-app/src/routes/+page.svelte

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<section class="offers">
+    <h1>Expernces of a life time</h1>
+    <p>Here at Tambotie we provide 3 once in a lifetime opptunities</p>
+
+    <div class="row">
+        <div class="collums">
+            <h3>Wildlife Rehabilitation Center</h3>
+            <p>An absolute must to visit and experience.
+                Daily Tours: Mon – Sat (09h30 and 15:00),
+                Open as usual during school holidays and public holidays (09h30 and 15:00),
+                Sundays at 15h00 during school holidays and long weekends</p>
+            <p class="phone">Phone: 028 851 2272</p>
+            <p class="email">Email: tambotie@worldonline.co.za</p>
+        </div>
+        <div class="collums">
+            <h3>Courses</h3>
+            <p>Whether you are pursuing a career in conservation 
+                or looking to further your knowledge as nature 
+                enthusiast, Tambotie has the perfect course for you.</p>
+            <p class="phone">Phone: 028 907 5983</p>
+            <p class="email">Email: tambotiecourses@worldonline.co.za</p>
+        </div>
+        <div class="collums">
+            <h3>Junior Fagasa</h3>
+            <p>Tambotie is proud to announce that we have partnered with junior fagasa. 
+                Helping educate the young leaders of the future and setting them on the path
+                to help save the wildlife.
+            </p>
+            <p class="phone">Phone: 028 748 4526</p>
+            <p class="email">Email: juniortambotie@worldonline.co.za</p>
+        </div>
+    </div>
+</section>
+
+<section class="location">
+    <h1>Where are we?</h1>
+    <p>Tambotie Wildlife Rehabilitation Center is located in a small town called Hoedspruit 
+        in Limpopo</p>
+
+        <div class="row">
+            <div class="location-col">
+              <img class="river" src="Images/hoedspruit.jpg" alt="Hoedspruit">  
+            </div>
+            <div class="location-col">
+                <img class="map" src="Images/map.png" alt="Map">
+              </div>
+        </div>
+</section>
+
+<section class="activities">
+    <h1>Rehab Activities</h1>
+    <p>Tambotie Wildlife Rehab Center, a  non-profit fueled by<br> 
+        community generosity and visitor support, offers engaging<br> <!--<br> is a line break -->
+        activities where every participation directly aids our<br> 
+        mission of caring for sick and injured animals</p>
+
+        <div class="row">
+            <div class="activities-col">
+                <img src="Images/valture.jpg" alt="Vulture">
+                <h3>Vulture  feeding</h3>
+                <p>Have a chance to feed one of the biggest birds on the earth</p>
+            </div>
+            <div class="activities-col">
+                <img src="Images/cheeta.jpg" alt="Cheetah">
+                <h3>Umfula</h3>
+                <p>Umfula is the girl that started it all.<br> 
+                    Shes the oldest and sweetest on the block.<br> 
+                    she loves a good cuddle</p>
+            </div>
+            <div class="activities-col">
+                <img src="Images/walk.jpg" alt="Rhino">
+                <h3>Walks with hamba</h3>
+                <p>Join hamba on one of his many daily walks throw the bush</p>
+            </div>
+        </div>
+
+</section>
+
+<section class="contact">
+    <h1> Contact us</h1>
+    <a href="contact" class="Contact-button">Get in contact</a>
+</section>

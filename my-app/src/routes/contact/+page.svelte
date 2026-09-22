e352e562844abc23bd3fa9f70ddb2cbe13000057
Sophie-skyyye <131939901+Sophie-skyyye@users.noreply.github.com>
2026-09-22 18:44:18 +1200

    Add my-app/src/routes/contact/+page.svelte

---
+++ my-app/src/routes/contact/+page.svelte
@@ -0,0 +1,25 @@
+<section class="offers">
+    <h1>Contact Info</h1>
+    <div class="row">
+        <div class="collums">
+            <h3>Wildlife Rehabilitation Center</h3>
+            <p>Office hours are 9 - 5pm for Rehab tours</p>
+            <p>If you have an animal in need please contact the emergency line</p>
+            <p class="phone">Phone: 028 851 2272</p>
+            <p class="email">Email: tambotie@worldonline.co.za</p>
+            <p>emergency line (24h): 028 675 8739</p>
+        </div>
+        <div class="collums">
+            <h3>Courses</h3>
+            <p>For more info please contact us at</p>
+            <p class="phone">Phone: 028 907 5983</p>
+            <p class="email">Email: tambotiecourses@worldonline.co.za</p>
+        </div>
+        <div class="collums">
+            <h3>Junior Fagasa</h3>
+            <p> For more info please contact us at </p>
+            <p class="phone">Phone: 028 748 4526</p>
+            <p class="email">Email: juniortambotie@worldonline.co.za</p>
+        </div>
+    </div>
+</section>

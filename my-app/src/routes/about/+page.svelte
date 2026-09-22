af56ae9f310d47099a4070c5b1161ab983e37632
Sophie-skyyye <131939901+Sophie-skyyye@users.noreply.github.com>
2026-09-22 18:43:54 +1200

    Add my-app/src/routes/about/+page.svelte

---
+++ my-app/src/routes/about/+page.svelte
@@ -0,0 +1,50 @@
+<section class="about">   
+    <h1>About Tambootie</h1>
+
+    <h3>Our Story<br> by Tom Van De Burg </h3>
+    <p>
+        Drought in the lowveld is a common occurrence and rainfall in the Hoedspruit area has always been patchy.<br>
+        In 2016 Hoedspruit was going though one of its many droughts.However it was the 12 March 2016 that would<br> 
+        have be the start of a crazy adventure.Every day at 6 am I get up and go for a walk with my dogs on my farm.<br>
+        I decides to take a different rout this time, which would take me past a very dried up river bed. I used to<br>
+        walk past the river every day but since its dried up, no more animals go to visit.<br> 
+        <br>
+        The drought was tough that year. Lots of animals were left hungry and thirsty. Often farmers where so despite<br>
+        they were building watering holes and setting up food troffs for their animals hoping to delay their deaths.<br>
+        A drought is often mother natures cull. Slowly the plants start to die, leaving the herbivors with little food<br>
+        making them weak and slow, the predators start to then eat more since they are able to catch more but, slowly<br> 
+        there food starts to disappears. Most preditors will move on try find another farm with more food and water.<br> 
+        Some start to fight each other for teritory and food.<br> 
+        <br>
+        In the middle of river bed is a massive Tambotie tree. Providing shade, food and homes to many animals.<br> 
+        My dogs start heardling towards the tree barking going crazy. Its not unusual for my dogs to go off ahead<br> 
+        or into the bush, however it was unusual for them to just be barking at one thing. 'oh kak' I thought to myself<br> 
+        they've found a carcase or some small animal. I rush over to find a very small what I thought at the time was a<br>
+        baby leopard but was actually a cheetah. Thinking the mother is a round i grab my dogs and leave. i keep walking<br>
+        until I get a call from one of my workers on the farm telling that they have just shot and killed a cheetah that<br> 
+        had broken into one of their homes in the compound, attacking one of the villagers. I didn't have cheetah on my<br> 
+        farm so it must have crossed over looking for some food. It was probably so despite for food it went into the village.<br>
+        With the Tambotie tree still in sight I made a very crazy choice to turn around and go get the baby. There was<br> 
+        no way it would have survived without its mother.<br>
+        <br>
+        3 years later I have a very big female cheetah, Umfula which means river in zulu. Umfula was basically raised by<br> 
+        the family dogs. She would come for walks in the morning, cuddle on the couch, play with the dogs and in the morning<br> 
+        she would force her way into the bed and under the bed sheets with the dogs.<br> 
+        <br>
+        Eventually the word got out that I had a cheetah which I cant have unless I had a Rehabilitation license and proper<br> 
+        grounds to house the animal. Umfula was seized and taking to a zoo in joburg.<br>
+        <br>
+        After many empty morning without Umfula i said screw it I'm going to have a Rehabilitation center. It took me 2 years<br>
+        to get my Rehabilitation license while I was doing my license i was building the Rehabilitation center. I had volunteers<br> 
+        helping and lots of people who love Umfula where donating.<br> 
+        <br>
+        On 12 of November 2021 Tambotie Wildlife Rehabilitation Center was opened and had its first member Umfula.
+    </p>
+</section>
+
+
+<style>
+    .about{
+        text-align: center;
+    }
+</style>
